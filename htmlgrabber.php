ce64fab2199da2563ab698f32005a15a1def1087
rob.tranquillo <rob.tranquillo@gmx.de>
2015-05-19 23:58:57 +0200

    fix error with linebreaks in titles and fix broken html->scrapeinfo function

--- htmlgrabber.php
+++ htmlgrabber.php
@@ -13,7 +13,7 @@ last update: 7 May 2015
 ///////////////////////////////
 // returns the needed tag from a string
 // on false returns false
-// [opt] limit = reand only to this length 
+// [opt] limit = read only to this length 
 function get_html_tag($tag, $b, $limit = -1)
 {
 	$tag_open = '<'.$tag.'>';
@@ -25,6 +25,8 @@ function get_html_tag($tag, $b, $limit = -1)
 	if($limit > 0 && $tagstart > $limit) return; //break if limit is reached
 	
 	$tag = substr( $b, $tagstart, $taglen );
+	$tag = str_replace(array("\r", "\n"), " ", $tag);
+	
 	if( $tagstart > 0 && $tagend > $tagstart ) return $tag; //return tag, even if tag is empty
 	else return false;
 }
--- pdfmetascapeinfo.php
+++ pdfmetascapeinfo.php
@@ -10,7 +10,7 @@ include('htmlgrabber.php'); //supports some html grabbing functions
 
 // basic example
 $downloadpath = 'downloads';
-$temppath = 'pdftooutput'; //all files will be delete!!!
+$temppath = 'pdftooutput'; //Attention, all files in there will be delete!
 
 $files = get_pdf_files($downloadpath);
 $all = count($files);
@@ -32,21 +32,25 @@ foreach( $files AS $document )
 function put_metadata_to_scrapeinfo($htmlinfo, $document)
 {		
 	///// look for .scrapeinfo file to update
-	$scrapeinfo = str_replace(array('.zip','.pdf','.png','.jpg'), '.scrapeinfo', $document);
-	if( file_exists( $scrapeinfo ) ) 
+	$scrapeinfopath = str_replace(array('.zip','.pdf','.png','.jpg'), '.scrapeinfo', $document);
+	if( file_exists( $scrapeinfopath ) ) 
 	{
 		//// edit the metadata into the scrapeinfo file
-		$scrapeinfo_arr = parse_ini_file( $scrapeinfo );
-		foreach($htmlinfo AS $key => $info)
+		$scrapeinfo = parse_ini_file( $scrapeinfopath, false, INI_SCANNER_RAW );
+		$new_scrapeinfo = array_merge( $scrapeinfo, $htmlinfo);
+		$newlines = '';
+		foreach($new_scrapeinfo AS $key => $val) 
 		{
-			$scrapeinfo_arr[$key] = $info;
+			$newlines .= "$key = $val\n";
 		}
-		file_put_contents( $scrapeinfo, $lineTitel, FILE_APPEND);
+		file_put_contents( $scrapeinfopath, $newlines);
 	}
 	else echo "ScrapeInfoFile not found: $scrapeinfo";
 }
 
 ////////////////////////////////////////////
+/*
+ */
 function get_metadata($htmlfile)
 {
 	if( file_exists($htmlfile) )
@@ -56,38 +60,10 @@ function get_metadata($htmlfile)
 		$buffer = fread($handle, 4096);
 		// get title
 		$title = get_html_tag('title', $buffer);
-		if(strlen($title)>0) $infos['title'] = $title;
+		if(strlen($title)>0) $infos['meta_title'] = $title;
 	}
 	return $infos;
 }
-
-////////////////////////////////////////////
-function OLD_____put_metadata_to_scrapeinfo($htmlfile, $document)
-{
-	if( file_exists($htmlfile) )
-	{
-		///// get the title from html file
-		$handle = @fopen($htmlfile, "r");
-		$buffer = fread($handle, 4096);
-		$title = get_html_tag('title', $buffer);
-		
-		///// look for .scrapeinfo file to update
-		$scrapeinfo = str_replace(array('.zip','.pdf','.png','.jpg'), '.scrapeinfo', $document);
-		if( file_exists( $scrapeinfo ) ) 
-		{
-			//// edit the metadata into the scrapeinfo file
-			$f = file_get_contents( $scrapeinfo );
-			if( substr_count($f, 'meta_title = ') == 0 && strlen($title) > 0 ) 
-			{
-				$lineTitel = "\nmeta_title = $title";
-				file_put_contents( $scrapeinfo, $lineTitel, FILE_APPEND);
-			}
-		}
-		else echo "ScrapeInfoFile not found: $scrapeinfo";
-	}
-}
-
-
 
 ////////////////////////////
 // pdftohtml with option -i for no images
